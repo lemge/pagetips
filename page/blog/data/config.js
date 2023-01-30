f0c850b773cfe637be9d917f3ff02eb79c46bc9e
lec <tgoding@live.com>
2023-01-30 13:33:55 +0800

    完成js填充内容

---
+++ page/blog/data/config.js
@@ -0,0 +1,19 @@
+var blogConfig={
+    webTitle:"博客标题",
+    botSlogan:"所愿结成 PageTips",
+    naviList:[
+        {
+            t:"首页",
+            l:"index.html"
+        },
+        {
+            t:"列表",
+            l:"list.html"
+        },
+        {
+            t:"关于",
+            l:"content.html"
+        }
+    ]
+
+}
---
+++ page/blog/src/main.js
@@ -0,0 +1,49 @@
+var Found={
+    fillData:[],
+    domList:{},
+    run:function (){
+        this.initDomList();
+      this.initFillData();
+
+    },
+    initFillData:function (){
+        if(blogConfig){
+            this.fillData.push(blogConfig)
+        }
+        this.fillCont();
+        return true;
+    },
+    initDomList:function (){
+        let dl=document.querySelectorAll(".jss")
+        for(let i=0;i<dl.length;i++){
+            this.domList[dl[i]]
+        }
+    },
+    struLinkList:function (l,domObj){
+        for(let i=0;i<l.length;i++){
+            var tmpli=document.createElement("li")
+            var tmpa=document.createElement("a")
+            tmpa.innerHTML=l[i]["t"]
+            tmpa.setAttribute("href",l[i]["l"])
+            tmpli.append(tmpa)
+            domObj.append(tmpli)
+        }
+        return domObj
+    },
+    fillCont:function (){
+        for(let i=0;i<this.fillData.length;i++){
+            let tmpDict=this.fillData[i]
+            for(var key in tmpDict){
+                if(key =="naviList"){
+                    this.struLinkList(tmpDict[key],document.querySelector("#"+key))
+                }else{
+                    document.querySelector("#"+key).innerHTML=tmpDict[key]
+                }
+            }
+        }
+    }
+}
+window.onload=function (){
+
+    Found.run()
+}
