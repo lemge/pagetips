a17320a9566392dbad614d9fcb4a32416cae0e85
lec <tgoding@live.com>
2023-02-02 15:27:33 +0800

    set the blog list

--- page/blog/src/main.js
+++ page/blog/src/main.js
@@ -1,5 +1,6 @@
 var Found={
     fillData:[],
+    blogListData:[],
     domList:{},
     run:function (){
         this.initDomList();
@@ -10,7 +11,11 @@ var Found={
         if(blogConfig){
             this.fillData.push(blogConfig)
         }
-        this.fillCont();
+        if(typeof(blogList)!=="undefined"){
+            this.blogListData=blogList;
+        }
+        this.fillCont();//填充博客配置内容
+        this.struBlogList();//填充博客列表
         return true;
     },
     initDomList:function (){
@@ -32,13 +37,57 @@ var Found={
         }
         return domObj
     },
+    struBlogList:function (){
+        if(this.blogListData && this.domList["blogList"])
+        {
+            console.log("has blog list")
+        }else{
+            console.log("no blog list")
+            return false;
+        }
+        let bl=this.domList["blogList"]
+        console.log(bl)
+        console.log(this.blogListData)
+        if (this.blogListData.length<1){
+            bl.innerHTML="没有数据"
+        }else{
+            for(let i=0;i<this.blogListData.length;i++){
+                let ele_data=this.blogListData[i]
+                var lidom=document.createElement("li")
+                var adom=document.createElement("a")
+                adom.setAttribute("href","content.html?untid="+ele_data['untid'])
+                lidom.append(adom)
+
+                var titlediv=document.createElement("div")
+                titlediv.classList.add("list-title")
+                titlediv.innerHTML=ele_data['title']
+                adom.append(titlediv)
+
+                var txtdiv=document.createElement('div')
+                txtdiv.classList.add("list-txt")
+                txtdiv.innerHTML=ele_data['txt']
+                adom.append(txtdiv)
+
+                var imgdiv=document.createElement("div")
+                imgdiv.classList.add("list-img")
+                adom.append(imgdiv)
+
+                var imgobj=document.createElement("img")
+                imgobj.setAttribute("src",ele_data["cover"])
+
+
+            }
+        }
+        return true;
+    },
     fillCont:function (){
         for(let i=0;i<this.fillData.length;i++){
             let tmpDict=this.fillData[i]
             for(var key in tmpDict){
                 if(key =="naviList"){
                     this.struLinkList(tmpDict[key],this.domList[key])
-                }else{
+                }
+                else{
                     this.domList[key].innerHTML=tmpDict[key]
                 }
             }
